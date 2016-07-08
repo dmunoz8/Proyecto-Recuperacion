afe2bf3486bba22e678653d6a06658b3814d1f00
jeffryvenegas <jeffvene@gmail.com>
2016-07-07 22:52:16 -0600

    Extrae Portadas en imgs/

--- RecuInfo/recuinfo/ImgIndex.py
+++ RecuInfo/recuinfo/ImgIndex.py
@@ -3,18 +3,29 @@
 # and open the template in the editor.
 import rarfile
 import os
+import shutil
 
-def extractFirstFile(src, dest):
+def extractFirstFile(src, dest, titulo, normalizado):
 	with rarfile.RarFile(src) as rf:
-		portada = rf.infolist()[0].filename
-		rf.extract(portada, path=dest, pwd=None)  
-
+		lista = rf.infolist()
+		portada = lista[0].filename #.jpg que contiene la portada
+		dirPadre = lista[-1].filename
+		rf.extract(portada, path=dest, pwd=None)
+		#Se cambia nombre de la portada
+		origName = "./imgs/"+portada
+		newName = "./imgs/"+dirPadre+"/"+normalizado+".jpg"
+		os.rename(origName,newName)
+		shutil.move(newName,"./imgs")
+		os.rmdir("./imgs/"+dirPadre)
 
 def getComicsCovers(srcPath):
 	carpetas = os.listdir(srcPath)
 	for titulo in carpetas:
 		for filename in os.listdir(srcPath+"/"+titulo):
-			extractFirstFile(srcPath+"/"+titulo+"/"+filename,"./imgs")
+			normalizado = filename[:-4]
+			fullSrc = srcPath+"/"+titulo+"/"+filename
+			extractFirstFile(fullSrc,"./imgs",titulo,normalizado)
+
 		print(titulo+"/ procesado...")
 
 if __name__ == "__main__":
